2d98815a808129cfc878c61e7257b751f1ac2c22
John Muhoza <muhozajohn250@gmail.com>
2025-06-03 10:28:02 +0200

    feat: refactor store setup and add JobsPage component for job listings

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/index";
 
-const store = configureStore({
-	reducer: rootReducer,
-	devTools: process.env.NODE_ENV !== "production",
-});
+const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    devTools: process.env.NODE_ENV !== "production",
+  });
+};
+
+const store = makeStore();
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
